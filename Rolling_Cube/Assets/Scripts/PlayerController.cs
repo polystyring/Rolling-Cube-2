c127a6cc17d25b590a1fc91973842374ba8b4ce3
Eric Knittel <knittel.eric@gmail.com>
2016-03-17 19:15:05 -0400

    + Added wall jump (front and sides raycasting)

--- Rolling_Cube/Assets/Scripts/PlayerController.cs
+++ Rolling_Cube/Assets/Scripts/PlayerController.cs
@@ -7,31 +7,37 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rbodyToMove;
 
     public float speed, rotationSpeed;
-    private float defaultSpeed, defaultRotationSped, fallingSpeed, fallTimer;
+    private float defaultSpeed, defaultRotationSpeed, fallingSpeed, fallTimer;
     public float jumpPowerUp, jumpPowerFoward, jumpPowerBack, jumpPowerLeftRight;
+    public float wallJumpPowerUp, wallJumpPowerFoward, wallJumpPowerBack, wallJumpPowerLeftRight;
     public float jumpMoveSpeed, jumpMoveTurn;
-    public bool isGrounded, isJumping;
-    public bool canJump, canTwist, canSlide;   
+    public bool isGrounded, isTouchingFront, isTouchingLeft, isTouchingRight, isTouchingWall, isJumping, isFalling;
+    public bool canJump, canTwist, canSlide;
 
-    public RaycastHit[] blackBoxList = new RaycastHit[4];
+    RaycastHit[] belowBlackBoxList = new RaycastHit[4];
+    RaycastHit[] frontBlackBoxList = new RaycastHit[4];
+    RaycastHit[] leftBlackBoxList = new RaycastHit[4];
+    RaycastHit[] rightBlackBoxList = new RaycastHit[4];
 
     float inputVertical, inputHorizontal;
 
-    float scaledSpeed, inverseSpeed;
     public bool isTwisting = false;
-    float twistTime;
+    bool twistingLeft = false, twistingRight = false;
     public float twistSpeed;
+    Quaternion twistTarget;
+    float twistAngle;
 
-    public float slideSpeed;
     public bool isSliding = false;
+    public float slideSpeed;
     float slideTime;
 
+    float timeCheck;
 
 
     void Start()
     {
         defaultSpeed = speed;
-        defaultRotationSped = rotationSpeed;
+        defaultRotationSpeed = rotationSpeed;
 
         //canJump = false;
         //canTwist = false;
@@ -44,64 +50,151 @@ public class PlayerController : MonoBehaviour
     {
         inputVertical = Input.GetAxis("Vertical");
         inputHorizontal = Input.GetAxis("Horizontal");
-
-        //print(inputVertical + "Vertical");
-        //print(inputHorizontal + "Horizontal");
-
-        //        var upSpeed = Vector3.Dot(rbodyToMove.velocity, rbodyToMove.transform.up);
-        //        var downSpeed = Vector3.Dot(rbodyToMove.velocity, -rbodyToMove.transform.up);
-        //        var rightSpeed = Vector3.Dot(rbodyToMove.velocity, rbodyToMove.transform.right);
-        //        var leftSpeed = Vector3.Dot(rbodyToMove.velocity, -rbodyToMove.transform.right);
-        //        var forwardSpeed = Vector3.Dot(rbodyToMove.velocity, rbodyToMove.transform.forward);
-        //        var backwardSpeed = Vector3.Dot(rbodyToMove.velocity, -rbodyToMove.transform.forward);
-
-        //print(leftSpeed);
+        
 
         isGrounded = false;
 
         Ray groundCheckNW = new Ray(transform.TransformPoint(-.5f, 0, .5f), -transform.up);
-        Physics.Raycast(groundCheckNW, out blackBoxList[0], .6f);
+        Physics.Raycast(groundCheckNW, out belowBlackBoxList[0], .6f);
 
         Ray groundCheckNE = new Ray(transform.TransformPoint(.5f, 0, .5f), -transform.up);
-        Physics.Raycast(groundCheckNE, out blackBoxList[1], .6f);
+        Physics.Raycast(groundCheckNE, out belowBlackBoxList[1], .6f);
 
         Ray groundCheckSW = new Ray(transform.TransformPoint(-.5f, 0, -.5f), -transform.up);
-        Physics.Raycast(groundCheckSW, out blackBoxList[2], .6f);
+        Physics.Raycast(groundCheckSW, out belowBlackBoxList[2], .6f);
 
         Ray groundCheckSE = new Ray(transform.TransformPoint(.5f, 0, -.5f), -transform.up);
-        Physics.Raycast(groundCheckSE, out blackBoxList[3], .6f);
+        Physics.Raycast(groundCheckSE, out belowBlackBoxList[3], .6f);
 
-        for (int i = 0; i < blackBoxList.Length && isGrounded == false; i++)
+        for (int i = 0; i < belowBlackBoxList.Length && !isGrounded; i++)
         {
-            if (blackBoxList[i].collider != null && blackBoxList[i].collider.gameObject.layer != 8)
+            if (belowBlackBoxList[i].collider != null && belowBlackBoxList[i].collider.gameObject.layer != 8)
             {
                 isGrounded = true;
+                isFalling = false;
                 fallingSpeed = 0;
                 fallTimer = 0;
             }
         }
 
-        //foreach (RaycastHit r in blackBoxList)
-        //    print(r.collider);
 
-        //print(isGrounded);
+        isTouchingFront = false;
+
+        Ray frontCheckNW = new Ray(transform.TransformPoint(-.5f, .5f, 0), transform.forward);
+        Physics.Raycast(frontCheckNW, out frontBlackBoxList[0], .65f);
+
+        Ray frontCheckNE = new Ray(transform.TransformPoint(.5f, .5f, 0), transform.forward);
+        Physics.Raycast(frontCheckNE, out frontBlackBoxList[1], .65f);
+
+        Ray frontCheckSW = new Ray(transform.TransformPoint(-.5f, -.45f, 0), transform.forward);
+        Physics.Raycast(frontCheckSW, out frontBlackBoxList[2], .65f);
+
+        Ray frontCheckSE = new Ray(transform.TransformPoint(.5f, -.45f, 0), transform.forward);
+        Physics.Raycast(frontCheckSE, out frontBlackBoxList[3], .65f);
+
+        for (int i = 0; i < frontBlackBoxList.Length; i++)
+        {
+            if (frontBlackBoxList[i].collider != null && frontBlackBoxList[i].collider.gameObject.layer != 8)
+            {
+                isTouchingFront = true;
+            }
+        }
+
+
+        isTouchingLeft = false;
+
+        Ray leftCheckNW = new Ray(transform.TransformPoint(0, .5f, -.5f), -transform.right);
+        Physics.Raycast(leftCheckNW, out leftBlackBoxList[0], .65f);
+
+        Ray leftCheckNE = new Ray(transform.TransformPoint(0, .5f, .5f), -transform.right);
+        Physics.Raycast(frontCheckNE, out leftBlackBoxList[1], .65f);
+
+        Ray leftCheckSW = new Ray(transform.TransformPoint(0, -.45f, -.5f), -transform.right);
+        Physics.Raycast(leftCheckSW, out leftBlackBoxList[2], .65f);
+
+        Ray leftCheckSE = new Ray(transform.TransformPoint(0, -.45f, .5f), -transform.right);
+        Physics.Raycast(leftCheckSE, out leftBlackBoxList[3], .65f);
+
+        for (int i = 0; i < leftBlackBoxList.Length; i++)
+        {
+            if (leftBlackBoxList[i].collider != null && leftBlackBoxList[i].collider.gameObject.layer != 8)
+            {                
+                isTouchingLeft = true;
+            }
+
+        }
+
+
+        isTouchingRight = false;
+
+        Ray rightCheckNW = new Ray(transform.TransformPoint(0, .5f, .5f), transform.right);
+        Physics.Raycast(rightCheckNW, out rightBlackBoxList[0], .65f);
+
+        Ray rightCheckNE = new Ray(transform.TransformPoint(0, .5f, -.5f), transform.right);
+        Physics.Raycast(rightCheckNE, out rightBlackBoxList[1], .65f);
+
+        Ray rightCheckSW = new Ray(transform.TransformPoint(0, -.45f, .5f), transform.right);
+        Physics.Raycast(rightCheckSW, out rightBlackBoxList[2], .65f);
+
+        Ray rightCheckSE = new Ray(transform.TransformPoint(0, -.45f, -.5f), transform.right);
+        Physics.Raycast(rightCheckSE, out rightBlackBoxList[3], .65f);
+
+        for (int i = 0; i < rightBlackBoxList.Length; i++)
+        {
+            if (rightBlackBoxList[i].collider != null && rightBlackBoxList[i].collider.gameObject.layer != 8)
+            {                
+                isTouchingRight = true;
+            }
+
+        }
+
+        if (isTouchingFront || isTouchingLeft || isTouchingRight)
+            isTouchingWall = true;
+        else
+            isTouchingWall = false;
+
+
+        //Debug.DrawRay(transform.TransformPoint(0, .5f, .5f), transform.right, Color.red, 2);
+        //Debug.DrawRay(transform.TransformPoint(0, .5f, -.5f), transform.right, Color.blue, 2);
+        //Debug.DrawRay(transform.TransformPoint(0, -.45f, .5f), transform.right, Color.green, 2);
+        //Debug.DrawRay(transform.TransformPoint(0, -.45f, -.5f), transform.right, Color.yellow, 2);
+
 
         isJumping = false;
 
-        if (Input.GetButtonDown("Jump") && isGrounded && canJump)
+        if (Input.GetButtonDown("Jump"))
         {
-            isJumping = true;
+            if (isGrounded && canJump)
+            {
+                isJumping = true;
 
-            rbodyToMove.AddForce(transform.up * jumpPowerUp, ForceMode.Impulse);
+                rbodyToMove.AddForce(transform.up * jumpPowerUp, ForceMode.Impulse);
 
-            if (inputVertical > 0)
-                rbodyToMove.AddForce(transform.forward * inputVertical * jumpPowerFoward, ForceMode.Impulse);
-            if (inputVertical < 0)
-                rbodyToMove.AddForce(transform.forward * inputVertical * jumpPowerBack, ForceMode.Impulse);
-            if (inputHorizontal > 0)
-                rbodyToMove.AddForce(transform.right * inputHorizontal * jumpPowerLeftRight, ForceMode.Impulse);
-            if (inputHorizontal < 0)
-                rbodyToMove.AddForce(transform.right * inputHorizontal * jumpPowerLeftRight, ForceMode.Impulse);
+                if (inputVertical > 0)
+                    rbodyToMove.AddForce(transform.forward * inputVertical * jumpPowerFoward, ForceMode.Impulse);
+                if (inputVertical < 0)
+                    rbodyToMove.AddForce(transform.forward * inputVertical * jumpPowerBack, ForceMode.Impulse);
+                if (inputHorizontal != 0)
+                    rbodyToMove.AddForce(transform.right * inputHorizontal * jumpPowerLeftRight, ForceMode.Impulse);
+            }
+
+            if (!isGrounded && isTouchingWall)
+            {
+                isJumping = true;
+
+                rbodyToMove.velocity = Vector3.zero;
+
+                rbodyToMove.AddForce(transform.up * wallJumpPowerUp, ForceMode.Impulse);
+
+                if (inputVertical > 0)
+                    rbodyToMove.AddForce(transform.forward * inputVertical * wallJumpPowerFoward, ForceMode.Impulse);
+                if (inputVertical < 0)
+                    rbodyToMove.AddForce(transform.forward * inputVertical * wallJumpPowerBack, ForceMode.Impulse);
+                if (inputHorizontal != 0)
+                    rbodyToMove.AddForce(transform.right * inputHorizontal * wallJumpPowerLeftRight, ForceMode.Impulse);
+            }
+
+
         }
 
         //print(rbodyToMove.velocity.magnitude);
@@ -109,44 +202,45 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButtonDown("Left Bumper") && canTwist && !isTwisting && !isSliding)
         {
-            scaledSpeed = rbodyToMove.velocity.magnitude / twistSpeed;
-            inverseSpeed = 1 - scaledSpeed;
-
-            rbodyToMove.velocity = Vector3.zero;
             rbodyToMove.angularVelocity = Vector3.zero;
-            rbodyToMove.AddTorque(transform.up * -15, ForceMode.Impulse);
-
+            twistTarget = Quaternion.Euler(transform.eulerAngles) * Quaternion.Euler(0, -90, 0);
             isTwisting = true;
-            twistTime = .5f;
-            rotationSpeed = 0;
-            speed = 0;
+            twistingLeft = true;
         }
 
         if (Input.GetButtonDown("Right Bumper") && canTwist && !isTwisting && !isSliding)
         {
-            scaledSpeed = rbodyToMove.velocity.magnitude / twistSpeed;
-            inverseSpeed = 1 - scaledSpeed;
-
-            rbodyToMove.velocity = Vector3.zero;
             rbodyToMove.angularVelocity = Vector3.zero;
-            rbodyToMove.AddTorque(transform.up * 15 * inverseSpeed, ForceMode.Impulse);
-
+            twistTarget = Quaternion.Euler(transform.eulerAngles) * Quaternion.Euler(0, 90, 0);
             isTwisting = true;
-            twistTime = .5f;
-            rotationSpeed = 0;
-            speed = 0;
+            twistingRight = true;
         }
 
-        if (isTwisting == true)
+        if (isTwisting)
         {
-            twistTime -= Time.deltaTime;
-            if (twistTime <= 0)
+            rotationSpeed = 0;
+            twistAngle = Quaternion.Angle(transform.rotation, twistTarget);
+
+            //print(twistAngle);
+
+            if (twistAngle <= 5)
             {
+                rbodyToMove.transform.rotation = twistTarget;
+                rbodyToMove.angularVelocity = Vector3.zero;
+                rotationSpeed = defaultRotationSpeed;
                 isTwisting = false;
-                rotationSpeed = defaultRotationSped;
-                speed = defaultSpeed;
+                twistingRight = false;
+                twistingRight = false;
+            }
+            else
+            {
+                if (twistingRight)
+                    rbodyToMove.AddTorque(transform.up * twistSpeed * Time.deltaTime, ForceMode.Impulse);
+                else
+                    rbodyToMove.AddTorque(transform.up * -twistSpeed * Time.deltaTime, ForceMode.Impulse);
             }
         }
+
 
 
         if (Input.GetButtonDown("Slide Left") && canSlide && !isSliding && !isTwisting)
@@ -197,7 +291,7 @@ public class PlayerController : MonoBehaviour
 
 
         float downSpeed = Vector3.Dot(rbodyToMove.velocity, -rbodyToMove.transform.up);
-        
+
 
         if (!isGrounded && downSpeed > 0)
         {
@@ -206,11 +300,12 @@ public class PlayerController : MonoBehaviour
             if (fallTimer > 3.5f)
             {
                 //print(downSpeed);
-                fallingSpeed += 2.5f;
+                isFalling = true;
+                fallingSpeed += 2f;
                 rbodyToMove.AddForce(-rbodyToMove.transform.up * fallingSpeed, ForceMode.Acceleration);
             }
         }
-            
+
 
 
 
